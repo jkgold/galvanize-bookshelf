40172059fea941cd7a225eb7090bb78e02412d0e
jasonkarlgoldfarb@gmail.com <jasonkarlgoldfarb@gmail.com>
2016-12-02 13:03:16 -0700

    seeding and migration done for part 3

---
+++ migrations/20161202115535_users.js
@@ -0,0 +1,16 @@
+'use strict'
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', function(table){
+    table.increments();
+    table.varchar('first_name').notNullable().defaultTo('');
+    table.varchar('last_name').notNullable().defaultTo('');
+    table.varchar('email').notNullable().unique();
+    table.specificType('hashed_password','char(60)').notNullable();
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.droptTable('users');
+
+};
